0cb4a6b587209e146992e86b7c780ab3b7d87170
jaymanale <jaymanale29@gmail.com>
2022-07-26 14:30:28 +0530

    Initial Commit

---
+++ src/main/java/com/springboot/api/prodapi/ProdApiApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.api.prodapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProdApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProdApiApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/springboot/api/prodapi/model/Employee.java
@@ -0,0 +1,42 @@
+package com.springboot.api.prodapi.model;
+
+import lombok.*;
+import org.hibernate.Hibernate;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
+@Entity
+@Table(name = "employees")
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email")
+    private String email;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        Employee employee = (Employee) o;
+        return id != null && Objects.equals(id, employee.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
+}
